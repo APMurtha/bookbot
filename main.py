60ee398a4ef54cbce8a4f4c287420ff5300eb3f0
APMurtha <amurtha@gmail.com>
2024-06-15 02:27:05 -0400

    progress up to word count

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_word_count(text)
+    print(f"{num_words} words found in the document")  
+
+def get_word_count(file):
+    words = file.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
