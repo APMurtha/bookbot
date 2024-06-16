22a41b4647efe64614207ae09c182312139eb68f
APMurtha <amurtha@gmail.com>
2024-06-15 23:24:07 -0400

    final submission

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_word_count(text)
-    print(f"{num_words} words found in the document")  
+    print(f"---Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n") 
+    num_letters = count_each_letter(text)
+    num_letters.sort(reverse=True, key=sort_on)
+    for letter in num_letters:
+        char = letter["char"]
+        count = letter["num"]
+        print(f"The letter \"{char}\" character was found {count} times ")
+        print("--- End report ---")
 
 def get_word_count(file):
     words = file.split()
@@ -11,6 +19,22 @@ def get_word_count(file):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_each_letter(text):
+    letter_count = {}
+    letter_dict = []
+    for char in text.lower():
+        if char in letter_count:
+            letter_count[char] += 1
+        elif char.isalpha():
+            letter_count[char] = 1
+    for char, count in letter_count.items():
+        new_dict = {"char": char, "num": count}
+        letter_dict.append(new_dict)
+    return letter_dict
+
+def sort_on(dict):
+    return dict["num"]
 
 
 main()
